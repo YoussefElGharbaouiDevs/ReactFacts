c765eadc8ced9c12c62a207f3ff20b18941b7a42
Youssef El gharbaoui <114226937+YoussefElGharbaouiDevs@users.noreply.github.com>
2022-10-18 10:59:40 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+import Navbar from './header';
+const App = () => {
+    return (
+        <div>
+        <Navbar />
+        </div>
+    );
+    }
+
+export default App;

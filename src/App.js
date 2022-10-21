9f8e77ab562572f6e3ff62eca557af0d092cab79
Youssef el gharbaoui <youssefelgharbaoui.pro@gmail.com>
2022-10-21 21:24:09 +0100

    updating the code structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'; 
 import Navbar from './header';
+import Main from './main';
+
 const App = () => {
     return (
         <div>
         <Navbar />
+        <Main />
         </div>
     );
     }
